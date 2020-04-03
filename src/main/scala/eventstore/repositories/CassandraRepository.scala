2cf48edd5a279605b07aa3977495b566664f6e00
hasael <elmanhasa@gmail.com>
2020-04-03 18:11:05 +0200

    Add cassandra queries implementations

--- src/main/scala/eventstore/repositories/CassandraRepository.scala
+++ src/main/scala/eventstore/repositories/CassandraRepository.scala
@@ -1,14 +1,27 @@
 package eventstore.repositories
 
+import java.util.UUID
+
 import eventstore.events.{PaymentAccepted, PaymentDeclined, PaymentPending}
 import io.getquill.{CassandraSyncContext, SnakeCase}
+import io.getquill._
 
 import scala.util.{Success, Try}
 
+case class Events(id: UUID, content: String)
+
 class CassandraRepository {
   lazy val ctx = new CassandraSyncContext(SnakeCase, "ctx")
-  def insertPaymentAccepted(paymentAccepted: PaymentAccepted): Try[Unit] = Success()
-  def insertPaymentDeclined(paymentDeclined: PaymentDeclined): Try[Unit] = Success()
-  def insertPaymentPending(paymentPending: PaymentPending): Try[Unit] = Success()
+
+  import ctx._
+
+  def insertPaymentAccepted(paymentAccepted: PaymentAccepted): Try[Unit] =
+    Try(ctx.run(query[Events].insert(lift(Events(UUID.randomUUID(), paymentAccepted.eventName)))))
+
+  def insertPaymentDeclined(paymentDeclined: PaymentDeclined): Try[Unit] =
+    Try(ctx.run(query[Events].insert(lift(Events(UUID.randomUUID(), paymentDeclined.eventName)))))
+
+  def insertPaymentPending(paymentPending: PaymentPending): Try[Unit] =
+    Try(ctx.run(query[Events].insert(lift(Events(UUID.randomUUID(), paymentPending.eventName)))))
 
 }
