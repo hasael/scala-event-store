96521b08bc7c9e45bdb3b1dbbc8e0b27269403d1
hasael <elmanhasa@gmail.com>
2020-04-07 20:45:19 +0200

    Update cassandra table columns

--- src/main/scala/eventstore/repositories/CassandraRepository.scala
+++ src/main/scala/eventstore/repositories/CassandraRepository.scala
@@ -4,11 +4,11 @@ import java.util.UUID
 
 import eventstore.events.{PaymentAccepted, PaymentDeclined, PaymentPending}
 import io.getquill.{CassandraSyncContext, SnakeCase}
-import io.getquill._
 
-import scala.util.{Success, Try}
+import scala.util.Try
 
-case class Events(id: UUID, content: String)
+case class Events(id: UUID, content: String, amount: BigDecimal, currency: String, eventType: String, paymentType: String,
+                  transactionId: UUID, transactionTime: String)
 
 class CassandraRepository {
   lazy val ctx = new CassandraSyncContext(SnakeCase, "ctx")
@@ -16,12 +16,15 @@ class CassandraRepository {
   import ctx._
 
   def insertPaymentAccepted(paymentAccepted: PaymentAccepted): Try[Unit] =
-    Try(ctx.run(query[Events].insert(lift(Events(UUID.randomUUID(), paymentAccepted.eventName)))))
+    Try(ctx.run(query[Events].insert(lift(Events(UUID.randomUUID(), paymentAccepted.eventName, paymentAccepted.amount, paymentAccepted.currency, "PaymentAccepted", paymentAccepted.paymentData.paymentType
+      , UUID.fromString(paymentAccepted.transactionId), paymentAccepted.transactionTime)))))
 
   def insertPaymentDeclined(paymentDeclined: PaymentDeclined): Try[Unit] =
-    Try(ctx.run(query[Events].insert(lift(Events(UUID.randomUUID(), paymentDeclined.eventName)))))
+    Try(ctx.run(query[Events].insert(lift(Events(UUID.randomUUID(), paymentDeclined.eventName, paymentDeclined.amount, paymentDeclined.currency, "PaymentDeclined", paymentDeclined.paymentData.paymentType,
+      UUID.fromString(paymentDeclined.transactionId), paymentDeclined.transactionTime)))))
 
   def insertPaymentPending(paymentPending: PaymentPending): Try[Unit] =
-    Try(ctx.run(query[Events].insert(lift(Events(UUID.randomUUID(), paymentPending.eventName)))))
+    Try(ctx.run(query[Events].insert(lift(Events(UUID.randomUUID(), paymentPending.eventName, paymentPending.amount, paymentPending.currency, "PaymentPending", paymentPending.paymentType,
+      UUID.fromString(paymentPending.transactionId), paymentPending.transactionTime)))))
 
 }
